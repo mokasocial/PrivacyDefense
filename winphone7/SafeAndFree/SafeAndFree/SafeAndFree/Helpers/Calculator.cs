2d6fc3a07bf0a705251a8b631c3503cf8ab56170
unknown <Mark@LookingGlass.(none)>
2011-10-22 14:22:18 -0700

    Merge branch 'master' of https://github.com/mokasocial/PrivacyDefense
    Conflicts:
    	winphone7/SafeAndFree/MapEditor/obj/x86/Debug/App.g.cs
    	winphone7/SafeAndFree/MapEditor/obj/x86/Debug/GenerateResource.read.1.tlog
    	winphone7/SafeAndFree/MapEditor/obj/x86/Debug/GenerateResource.write.1.tlog
    	winphone7/SafeAndFree/MapEditor/obj/x86/Debug/MainWindow.g.cs
    	winphone7/SafeAndFree/MapEditor/obj/x86/Debug/MapEditor.exe
    	winphone7/SafeAndFree/MapEditor/obj/x86/Debug/MapEditor.pdb
    	winphone7/SafeAndFree/MapEditor/obj/x86/Debug/MapEditor_MarkupCompile.cache
    	winphone7/SafeAndFree/SafeAndFree.suo
    	winphone7/SafeAndFree/SafeAndFree/SafeAndFree/obj/Windows Phone/Debug/ContentPipeline-{43AD8136-6271-4094-B5E3-AA829BAC7E2D}.xml
    	winphone7/SafeAndFree/SafeAndFree/SafeAndFree/obj/Windows Phone/Debug/SafeAndFree.csproj.FileListAbsolute.txt
    	winphone7/SafeAndFree/SafeAndFree/SafeAndFree/obj/Windows Phone/Debug/SafeAndFree.dll
    	winphone7/SafeAndFree/SafeAndFree/SafeAndFree/obj/Windows Phone/Debug/SafeAndFree.pdb
    	winphone7/SafeAndFree/SafeAndFree/SafeAndFree/obj/Windows Phone/Debug/XapCacheFile.xml

--- winphone7/SafeAndFree/SafeAndFree/SafeAndFree/Helpers/Calculator.cs
+++ winphone7/SafeAndFree/SafeAndFree/SafeAndFree/Helpers/Calculator.cs
@@ -21,5 +21,16 @@ namespace SafeAndFree.Helpers
         {
             return Math.Sqrt(Math.Pow(pos1.X - pos2.X, 2) + Math.Pow(pos1.Y - pos2.Y, 2));
         }
+
+
+        public static Vector2 MovementTowardsPoint(Vector2 start, Vector2 end, Vector2 nextTarget, int speed)
+        {
+            double totalDistance = GetDistance(start, end);
+            double movementPercent = (double)speed * totalDistance;
+            double xDiff = start.X - end.X;
+            double yDiff = start.Y - end.Y;
+            return new Vector2((float)(start.X + (xDiff * movementPercent)), (float)(start.Y + (yDiff * movementPercent)));
+        }
+
     }
 }
