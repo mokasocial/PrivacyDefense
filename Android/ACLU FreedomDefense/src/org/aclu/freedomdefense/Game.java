e1e0911e5e9c1c9a99dcf0323aaaf29125b50dea
Emory Myers <emorym@gmail.com>
2011-10-22 00:32:02 -0700

    HOW DO I GET THROUGH TO DEEZ KIDS

--- Android/ACLU FreedomDefense/src/org/aclu/freedomdefense/Game.java
+++ Android/ACLU FreedomDefense/src/org/aclu/freedomdefense/Game.java
@@ -1,41 +1,76 @@
 package org.aclu.freedomdefense;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 public class Game implements ApplicationListener
 {
-	@Override
+	private SpriteBatch batch;
+	private Texture spriteSheet;
+	private int[][] tiles;
+	
 	public void create()
 	{
+		batch = new SpriteBatch();
+		spriteSheet = new Texture( Gdx.files.internal( "sprite_sheet.png" ));
+		tiles = new int[30][20];
 		
+		for( int x = 0; x < 30; ++x )
+			for( int y = 0; y < 20; ++y )
+				tiles[x][y] = 4;
+		
+		for( int y = 19; y >= 0; y-- )
+			tiles[1][y] = 5;
+		
+		tiles[1][0] = 8;
+		
+		for( int x = 2; x < 6; ++x )
+		{
+			tiles[x][0] = 6;
+		}
+		
+		tiles[5][0] = 7;
+		
+		for( int y = 1; y < 18; ++y )
+		{
+			tiles[5][y] = 5;
+		}
 	}
 	
-	@Override
 	public void render()
 	{
-		
+		Gdx.gl.glClear( GL10.GL_COLOR_BUFFER_BIT ); // clear the screen
+		batch.begin();
+		// Draw the terrain!
+		for( int x = 0; x < 30; ++x )
+		{
+			for( int y = 0; y < 20; ++y )
+			{
+				batch.draw( spriteSheet, x*16, y*16, tiles[x][y]*16, 0, 16, 16 );
+			}
+		}
+		batch.end();
 	}
 	
-	@Override
 	public void resize( int width, int height )
 	{
 		
 	}
 	
-	@Override
 	public void pause()
 	{
 		
 	}
 	
-	@Override
 	public void resume()
 	{
 		
 	}
 	
-	@Override
 	public void dispose()
 	{
 		
