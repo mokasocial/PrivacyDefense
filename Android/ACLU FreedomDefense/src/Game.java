6b57795bd17fbfc45dfb31dfeaca4aa1d791aaba
Emory Myers <emorym@gmail.com>
2011-10-21 22:56:35 -0700

    I'M SO EXCITED

--- Android/ACLU FreedomDefense/src/Game.java
+++ Android/ACLU FreedomDefense/src/Game.java
@@ -1,15 +1,22 @@
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game implements ApplicationListener
 {
+	private SpriteBatch batch;
+	
 	public void create()
 	{
-		
+		batch = new SpriteBatch();
 	}
 	
 	public void render()
 	{
-		
+		Gdx.gl.glClear( GL10.GL_COLOR_BUFFER_BIT ); // clear the screen
+		batch.begin();
+		// Drawing goes here!
+		batch.end();
 	}
 	
 	public void resize( int width, int height )
