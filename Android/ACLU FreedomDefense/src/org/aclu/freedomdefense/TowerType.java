ed68adc3bf30afc0814ffbc4c2553b632ab06600
Michael Morris <morris4019@gmail.com>
2011-10-22 18:47:24 -0700

    Imlemented free towers on UI.

--- Android/ACLU FreedomDefense/src/org/aclu/freedomdefense/TowerType.java
+++ Android/ACLU FreedomDefense/src/org/aclu/freedomdefense/TowerType.java
@@ -1,17 +1,34 @@
 package org.aclu.freedomdefense;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class TowerType {
 
 	private final String m_towerType;
+	private final int m_price;
+	private final Vector2 sprite_loc;
 
-	private TowerType(String type) {
+	private TowerType(String type, int price, Vector2 the_sprite_loc) {
 		m_towerType = type;
+		m_price = price;
+		sprite_loc = the_sprite_loc;
 	}
 
-	public static TowerType JUDGE = new TowerType("JUDGE");
-	public static TowerType FIREWALL = new TowerType("FIREWALL");
-	public static TowerType TEACHER = new TowerType("TEACHER");
-	public static TowerType LAWSUIT = new TowerType("LAWSUIT");
+	public int getPrice() {
+		return m_price;
+	}
+
+	public int getSpriteLocX() {
+		return (int)sprite_loc.x;
+	}
+	public int getSpriteLocY() {
+		return (int)sprite_loc.y;
+	}
+	
+	public static TowerType JUDGE = new TowerType("JUDGE", 100, new Vector2( 9*16, 1*16));
+	public static TowerType FIREWALL = new TowerType("FIREWALL", 200, new Vector2( 10*16, 1*16));
+	public static TowerType TEACHER = new TowerType("TEACHER", 300, new Vector2( 10*16, 1*16));
+	public static TowerType LAWSUIT = new TowerType("LAWSUIT", 400, new Vector2( 10*16, 1*16));
 	
 	public static TowerType[] Values = {
 		JUDGE,
@@ -19,4 +36,5 @@ public class TowerType {
 		TEACHER,
 		LAWSUIT
 	};	
+	
 }
