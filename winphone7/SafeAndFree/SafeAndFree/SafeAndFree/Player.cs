374dc20576af647b0cdef7036f06f9c28e524b99
Kevin Stubbs <v-kestub@microsoft.com>
2011-10-22 19:58:07 -0700

    Added player and tile selection.

---
+++ winphone7/SafeAndFree/SafeAndFree/SafeAndFree/Player.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SafeAndFree
+{
+    /// <summary>
+    /// Logic for the player.
+    /// </summary>
+    class Player
+    {
+        /// <summary>
+        /// The number of lives of left.
+        /// </summary>
+        public int Lives { get; set; }
+
+        /// <summary>
+        /// The player's score.
+        /// </summary>
+        public int Score { get; set; }
+
+        /// <summary>
+        /// Returns true if the player has lost.
+        /// </summary>
+        public bool HasLost
+        {
+            get
+            {
+                return Lives > 0;
+            }
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Player()
+        {
+            Lives = 6;
+            Score = 0;
+        }
+    }
+}
